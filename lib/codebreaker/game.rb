f8568d121109344a92fe3e903fddeef65ab1d37e
Kelmer Perez <kelmerp@gmail.com>
2013-12-02 16:55:55 -0600

    refactored code into a new Marker class and added specs for MArker class

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,22 +11,9 @@ module Codebreaker
     end
 
     def guess(guess)
-      if exact_match?(guess, 0)
-        mark = '+'
-      elsif number_match?(guess, 0)
-        mark = '-'
-      else
-        mark = ''
-      end
-      @output.puts mark
-    end
-
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index])
+      marker = Marker.new(@secret, guess)
+      @output.puts '+' * marker.exact_match_count +
+       '-' * marker.number_match_count
     end
   end
 end
